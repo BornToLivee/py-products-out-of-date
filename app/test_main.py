c5372bb236c50edb26e5721ff0fc0200311135a1
Bohdan Zinchenko <bogdan.zinchenko.2019@gmail.com>
2024-06-03 18:01:11 +0300

    py-products-out-of-date v1

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,23 @@
-# write your code here
+from app.main import outdated_products
+import datetime
+
+
+def test_outdated_products() -> None:
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2024, 6, 3),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2024, 6, 2),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160
+        }
+    ]
+    assert outdated_products(products) == ["chicken", "duck"]
